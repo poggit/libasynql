a6de6c1f0c7c07814cc3215d5e75d278a44e4923
matcracker <matcracker98@gmail.com>
2020-02-03 22:07:58 +0100

    Improved exception error when .sql files are not preset in "resources" folder.

--- libasynql/src/poggit/libasynql/libasynql.php
+++ libasynql/src/poggit/libasynql/libasynql.php
@@ -33,6 +33,7 @@ use poggit\libasynql\mysqli\MysqliThread;
 use poggit\libasynql\sqlite3\Sqlite3Thread;
 use function array_keys;
 use function count;
+use TypeError;
 use function extension_loaded;
 use function implode;
 use function is_array;
@@ -143,7 +144,11 @@ final class libasynql{
 
 		$connector = new DataConnectorImpl($plugin, $pool, $placeHolder, $logQueries ?? !libasynql::isPackaged());
 		foreach(is_string($sqlMap[$dialect]) ? [$sqlMap[$dialect]] : $sqlMap[$dialect] as $file){
-			$connector->loadQueryFile($plugin->getResource($file));
+			$resource = $plugin->getResource($file);
+			if($resource === null){
+				throw new TypeError("Missing $file in resources directory of plugin.");
+			}
+			$connector->loadQueryFile($resource);
 		}
 		return $connector;
 	}
