95ae8904233b27b54b8d68c1ecd5115ce520f41f
matcracker <matcracker98@gmail.com>
2020-05-15 16:13:03 +0200

    Now should be fine -_-

--- libasynql/src/poggit/libasynql/libasynql.php
+++ libasynql/src/poggit/libasynql/libasynql.php
@@ -65,9 +65,9 @@ final class libasynql{
 	/**
 	 * Create a {@link DatabaseConnector} from a plugin and a config entry, and initializes it with the relevant SQL files according to the selected dialect
 	 *
-	 * @param Plugin              $plugin 	  the plugin using libasynql
+	 * @param Plugin              $plugin     the plugin using libasynql
 	 * @param mixed               $configData the config entry for database settings
-	 * @param string[]|string[][] $sqlMap 	  an associative array with key as the SQL dialect ("mysql", "sqlite") and value as a string or string array indicating the relevant SQL files in the plugin's resources directory
+	 * @param string[]|string[][] $sqlMap     an associative array with key as the SQL dialect ("mysql", "sqlite") and value as a string or string array indicating the relevant SQL files in the plugin's resources directory
 	 * @param bool                $logQueries whether libasynql should log the queries with the plugin logger at the DEBUG level. Default <code>!libasynql::isPackaged()</code>.
 	 * @return DataConnector
 	 * @throws SqlError if the connection could not be created
