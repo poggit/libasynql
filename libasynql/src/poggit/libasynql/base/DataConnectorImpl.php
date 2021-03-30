e428bdda1110613898857330e4a37b62850d284d
Matt <matcracker@users.noreply.github.com>
2021-03-30 18:03:45 +0200

    Fixed typo in array declaration

--- libasynql/src/poggit/libasynql/base/DataConnectorImpl.php
+++ libasynql/src/poggit/libasynql/base/DataConnectorImpl.php
@@ -200,7 +200,7 @@ class DataConnectorImpl implements DataConnector{
 	public function asyncSelect(string $queryName, array $args = []) : Generator{
 		$onSuccess = yield Await::RESOLVE;
 		$onError = yield Await::REJECT;
-		$this->executeSelect($queryName, $args, static function(array[] $rows, SqlColumnInfo $columns) use($onSuccess) : void{
+		$this->executeSelect($queryName, $args, static function(array $rows, SqlColumnInfo $columns) use($onSuccess) : void{
 			$onSuccess($rows);
 		}, $onError);
 		$rows = yield Await::ONCE;
@@ -210,7 +210,7 @@ class DataConnectorImpl implements DataConnector{
 	public function asyncSelectWithInfo(string $queryName, array $args = []) : Generator{
 		$onSuccess = yield Await::RESOLVE;
 		$onError = yield Await::REJECT;
-		$this->executeInsert($queryName, $args, static function(array[] $rows, SqlColumnInfo $columns) use($onSuccess) : void{
+		$this->executeInsert($queryName, $args, static function(array $rows, SqlColumnInfo $columns) use($onSuccess) : void{
 			$onSuccess([$rows, $columns]);
 		}, $onError);
 		$rows = yield Await::ONCE;
