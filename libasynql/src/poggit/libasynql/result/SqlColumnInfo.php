d7d064ec1ce62dc945011ee44db1d41ba4fa7e7e
Laith98Dev <spt.laithdev@gmail.com>
2025-12-20 14:50:16 +0300

    Allow integer values of column name in sqlite3

--- libasynql/src/poggit/libasynql/result/SqlColumnInfo.php
+++ libasynql/src/poggit/libasynql/result/SqlColumnInfo.php
@@ -34,12 +34,12 @@ class SqlColumnInfo{
 	private $name;
 	private $type;
 
-	public function __construct(string $name, string $type){
+	public function __construct(string|int $name, string $type){
 		$this->name = $name;
 		$this->type = $type;
 	}
 
-	public function getName() : string{
+	public function getName() : string|int{
 		return $this->name;
 	}
 
